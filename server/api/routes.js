be2ae751e3ccca9ea7f9c53d3b37c071805aa885
Brian Keane <lonesomewhistle@gmail.com>
2022-11-20 21:50:33 -0600

    add-a-song

--- server/api/routes.js
+++ server/api/routes.js
@@ -4,6 +4,7 @@ function addRoutes(app) {
   app.use("/v1/users", require("./user"));
   app.use("/v1/spins", require("./spin"));
   app.use("/v1/voiceTracks", require("./voiceTrack"));
+  app.use("/v1/songs", require("./song"));
 }
 
 module.exports = {
